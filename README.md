4afe0f2c65ee5a7eddd768933dcdb347cba682bf
jajoe <padawan_joseph@yahoo.fr>
2016-11-14 21:34:03 +0100

    Transform the python script into a function

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Project realized with Koisell to validate the course "Information Theory" with M
 Need 3 parameters : the name of the hash function (sha1, sha2, sha3 or md5), the value and the key.
 
 Ex: sh hmac.sh sha3 value key
+
+# Use of the script hmac_script.py
+
+You can test the script by calling it with 3 parameters : the name of the hash function (sha1, sha2, sha3 or md5), the value and the key.
+
+Ex: python hmac_script.py sha3 value key
+
+You can use too the function of this script, which is hmac\_encrypt(f, my\_msg, key) and which return a string.
--- src/hmac_script.py
+++ src/hmac_script.py
@@ -2,18 +2,22 @@ import hmac as hmac
 import hashlib as hashlib
 import sys
 
-if(len(sys.argv) != 4):
-    print("Error ! Need 3 arguments : hash function (sha1, sha2, sha3 or md5), msg and key")
-else:
-    if(sys.argv[1] == "sha1"):
-        answer = hmac.new(sys.argv[3], msg=sys.argv[2], digestmod=hashlib.sha1)
-    elif(sys.argv[1] == "sha2"):
-        answer = hmac.new(sys.argv[3], msg=sys.argv[2], digestmod=hashlib.sha224)
-    elif(sys.argv[1] == "sha3"):
-        answer = hmac.new(sys.argv[3], msg=sys.argv[2], digestmod=hashlib.sha512)
-    elif(sys.argv[1] == "md5"):
-        answer = hmac.new(sys.argv[3], msg=sys.argv[2], digestmod=hashlib.md5)
+def hmac_encrypt(f, my_msg, key):
+    if(f == "sha1"):
+        answer = hmac.new(key, msg=my_msg, digestmod=hashlib.sha1)
+    elif(f == "sha2"):
+        answer = hmac.new(key, msg=my_msg, digestmod=hashlib.sha224)
+    elif(f == "sha3"):
+        answer = hmac.new(key, msg=my_msg, digestmod=hashlib.sha512)
+    elif(f == "md5"):
+        answer = hmac.new(key, msg=my_msg, digestmod=hashlib.md5)
     else:
-        print("Hash function error. Hash function = sha1, sha2, sha3 or md5")
+        return "Hash function error."
 
-    print(answer.hexdigest())
+    return answer.hexdigest()
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 4):
+        print("Error ! Need 3 arguments : hash function (sha1, sha2, sha3 or md5), msg and key")
+    else:
+        print(hmac_encrypt(sys.argv[1], sys.argv[2], sys.argv[3]))
